2db247a483734641c3514c7a90e3e6b23bfae146
Dhwani <dhwanijain731@gmail.com>
2025-10-23 16:19:01 +0530

    updated py file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, render_template_string
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -43,5 +44,9 @@ def home():
             result = "⚠️ Invalid Input! Please enter 30 numeric values."
     return render_template_string(HTML, result=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default 5000
+    app.run(host="0.0.0.0", port=port)
+
